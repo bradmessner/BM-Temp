0af7a8e30fe33c816fda7393f46e2e86ced375f8
Brad D. Messner <bmessner@setonhill.edu>
2022-09-08 05:51:50 -0400

    Test

---
+++ Test 2/src/Testit.java
@@ -0,0 +1,22 @@
+
+public class Testit {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Hello");
+		
+		int speed;
+		int age = 25;
+		speed = 70;
+		char middleName = 'D';
+		
+		System.out.println("My age is " + age);
+		String firstName = "Brad";
+		
+		
+		
+
+	}
+
+}
